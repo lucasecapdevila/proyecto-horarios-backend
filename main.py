b771c5d3b17b95c75df073d95406fdf5e8b30d02
Lucas Capdevila <lcapdevila60@gmail.com>
2025-10-24 19:38:39 -0300

    Se agregan librerías necesarias y metodos principales

---
+++ main.py
@@ -0,0 +1,48 @@
+# --- Importación de librerías ---
+from fastapi import FastAPI, Depends, HTTPException, Query, status
+from sqlalchemy.orm import Session
+from typing import List, Optional
+from datetime import datetime
+from contextlib import asynccontextmanager
+
+# --- Importación de archivos locales ---
+from . import models, schemas
+from .database import SessionLocal, engine, Base
+
+# --- Importación del middleware de CORS ---
+from fastapi.middleware.cors import CORSMiddleware
+
+# --- Lifespan crea las tablas al iniciar la app ---
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  Base.metadata.create_all(bind=engine)
+  yield
+
+# --- Inicialización de la aplicación FastAPI ---
+app = FastAPI(
+  title="API de Horarios de Colectivos",
+  description="API para gestionar horarios de colectivos utilizando FastAPI y SQLAlchemy.",
+  version="1.0.0",
+  lifespan=lifespan
+)
+
+# --- Configuración del middleware de CORS ---
+origins = [
+  "http://localhost:5173",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+# --- Dependencia para obtener la sesión de la base de datos ---
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()

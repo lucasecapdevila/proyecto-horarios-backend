bf49a22d2ebf3cb34dea454e8763de5497635c96
Lucas Capdevila <lcapdevila60@gmail.com>
2025-10-24 20:20:10 -0300

    Se agregan endpoints de la API - Get

--- main.py
+++ main.py
@@ -46,3 +46,41 @@ def get_db():
     yield db
   finally:
     db.close()
+
+# --- Rutas de la API ---
+# ========== ENDPOINT RAÍZ ==========
+@app.get('/', tags=["Info"])
+async def raiz():
+  return {
+    "nombre": "API de Horarios de Colectivos",
+    "version": "1.0.0",
+    "descripción": "API REST para gestionar horarios de colectivos y conexiones utilizando FastAPI y SQLAlchemy.",
+    "documentación": "Visita /docs para la documentación interactiva con Swagger de la API.",
+    "endpoints": {
+      "GET /": "Información sobre la API.",
+      "GET /docs": "Documentación interactiva con Swagger.",
+      "App": {
+        "GET /horarios-por-recorrido/{id}?tipo_dia=...": "Lista de horarios para un recorrido específico y tipo de día.",
+        "GET /conexiones?tipo_dia=...": "Calcula las conexiones entre recorridos basándose en los horarios."
+      },
+      "Admin (CRUD)": {
+        "GET /lineas": "Lista todas las líneas de colectivos.",
+        "POST /lineas": "Crea una nueva línea de colectivo.",
+        "GET /recorridos": "Lista todos los recorridos.",
+        "POST /recorridos": "Crea un nuevo recorrido.",
+        "POST /horarios": "Crea un nuevo horario.",
+      }
+    }
+  }
+
+# ========== ENDPOINTS GET ==========
+@app.get('/lineas/', response_model=List[schemas.Linea], tags=["Admin"])
+def get_lineas(db: Session = Depends(get_db)):
+  lineas = db.query(models.Linea).all()
+  return lineas
+
+@app.get('/recorridos/', response_model=List[schemas.Recorrido], tags=["Admin"])
+def get_recorridos(db: Session = Depends(get_db)):
+  recorridos = db.query(models.Recorrido).all()
+  return recorridos
+
